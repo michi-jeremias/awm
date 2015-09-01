bde408c97c6f2f15bcf88bbe8fe3d610c4e7f09d
Michi Jeremias <michael.jeremias.home@gmail.com>
2015-09-01 23:21:23 +0200

    u

--- hexView.R
+++ hexView.R
@@ -3,6 +3,11 @@ library("vars") # var, vec estimation
 
 dats12 <- readEViews("data//dats_12.wf1")
 colnames(dats12)
+dats12 <- dats12[-92,]
+dats12 <- dats12[-91,]
 
 y <- diff(log(dats12[,1]),4)
 m <- diff(log(dats12[,2]),4)
+
+df <- data.frame(y,m)
+summary(VAR(df,type="const",lag.max = 2))
