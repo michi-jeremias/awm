92e2fb561f586b9b843747f8b54503e501100584
michi-jeremias <michael.jeremias.home@gmail.com>
2015-09-02 11:57:16 +0200

    neuordnung: load.R für das laden der daten unitroot.R für UR/ADF etc analyse

--- unitroot.R
+++ unitroot.R
@@ -13,3 +13,19 @@ lm(diff(prod)[2:length(diff(prod))]~prod[1:(length(prod)-2)]+diff(prod)[1:length
 # gleich wie
 summary(ur.df(prod, type="drift", lags=1))
 
+
+
+# UR Analyse für PYR aus AWM
+acf(PYR)
+acf(PYR, plot=FALSE)
+pacf(PYR)
+pacf(PYR, plot=FALSE)
+
+# ADF
+summary(ur.df(PYR, type="drift", lags=4))
+
+# ADF für 1st diff
+summary(ur.df(diff(PYR, differences = 1), type="drift", lags=4))
+
+# ADF für 2nd diff
+summary(ur.df(diff(PYR, differences = 2), type="drift", lags=4))
